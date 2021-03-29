872d90fb5017886604de07771b943838811a8ba6
4boysdontcry <hgy02166@gmail.com>
2021-03-29 15:01:29 +0900

    03.29 last

--- js/index.js
+++ js/index.js
@@ -1,12 +1,31 @@
-$(window).scroll(function() {
-    var scTop = $(this).scrollTop()
-    if(scTop > 200) {
-        if(!$('.header-wrapper').hasClass('active')) /* active를 가지고 있으면 아래의 효과 적용X (! = not) */
-        $('.header-wrapper').css('top', '-60px')
-        $('.header-wrapper').stop().animate({'top':0}, 100)
-        $('.header-wrapper').addClass('active')
-    }
-    else {
-        $('.header-wrapper').removeClass('active')
-    }
+// $(window).scroll(function() {
+//     var scTop = $(this).scrollTop()
+//     if(scTop > 200) {
+//         if(!$('.header-wrapper').hasClass('active')) /* active를 가지고 있으면 아래의 효과 적용X (! = not) */
+//         $('.header-wrapper').css('top', '-60px')
+//         $('.header-wrapper').stop().animate({'top':0}, 300)
+//         $('.header-wrapper').addClass('active')
+//     }
+//     else {
+//         $('.header-wrapper').removeClass('active')
+//     }
+// })
+
+
+$(window).scroll(function () {
+	var scTop = $(this).scrollTop()
+	if (scTop > 200) {
+		$('.header-wrapper').css('transform', 'translateY(-100%)')
+		$('.header-wrapper').css('transform')
+		$('.header-wrapper').addClass('active')
+		$('.header-wrapper').css('transform', 'translateY(0)')
+	} else {
+		$('.header-wrapper').css('transform', 'translateY(0)')
+		$('.header-wrapper').removeClass('active')
+	}
+})
+
+
+$('.mo-bars').click(function() {
+	$('.monavi-wrapper').toggleClass('active')
 })
